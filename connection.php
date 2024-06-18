c5dec80e3f224794dfe31c67cdb3286283a41a85
Danilo De Azevedo <danilodeaz@gmail.com>
2024-06-18 21:05:00 +0200

    Updated web host connection

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
 
 //local connection
-$conn = mysqli_connect("localhost","root","","php_project")
-        or die("Unable to connect to database");
+// $conn = mysqli_connect("localhost","root","","php_project")
+//         or die("Unable to connect to database");
 
 //webhost connection
-//$conn = mysqli_connect("127.0.0.1:3306","u108639952_bmdatabase","BM_Database1","u108639952_bmdatabase")
-//        or die("Unable to connect to database");        
+$conn = mysqli_connect("localhost","hardwzhq_user","BM_Database1","hardwzhq__bmdatabase")
+       or die("Unable to connect to database");        
 
 ?>
